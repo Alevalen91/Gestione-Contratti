a41623f2bb703ce6fc14e9281c616edd31e000a2
Maurizio <maurizio.ignizio@gmail.com>
2021-02-04 00:43:57 +0100

    Gestionale Sql

---
+++ GestionaleSql/src/gestionale/LoginException.java
@@ -0,0 +1,14 @@
+package gestionale;
+
+public class LoginException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public LoginException() {
+		super("Login error");
+	}
+	
+	public String toString() {
+		return getMessage() + ": username o password errata!";
+	}
+
+}
