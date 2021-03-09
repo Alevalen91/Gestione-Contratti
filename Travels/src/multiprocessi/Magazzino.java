b17a25eaced9ea7f9858b850f2164f91d001684c
Maurizio <maurizio.ignizio@gmail.com>
2021-03-09 17:46:01 +0100

    wait() e notify()

--- Travels/src/multiprocessi/Magazzino.java
+++ Travels/src/multiprocessi/Magazzino.java
@@ -1,22 +1,51 @@
 package multiprocessi;
 
 public class Magazzino {
-	
+
 	private int numProdotti;
 	private int idProdotto;
-	
+
+	// magazzino vuoto
+	private boolean mag_vuoto = true; 
+
 	public synchronized void put(int idProd) {
+
+		// Se il magazzino non è vuoto:
+		if (!mag_vuoto) 
+			try {
+				wait(); // fermo il Produttore 
+			}
+		catch (InterruptedException exc) {
+			exc.printStackTrace();
+		}
+
 		this.idProdotto = idProd;
 		numProdotti++;
 		stampaSituazione("Prodotto " + idProd);
+		
+		mag_vuoto = false; //magazzino pieno
+		notify(); //sveglio il Consumatore
 	}
 
 	public synchronized int get() {
+		
+		// Se il magazzino è vuoto:
+		if (mag_vuoto) 
+			try {
+				wait(); // fermo il Consumatore 
+			}
+		catch (InterruptedException exc) {
+			exc.printStackTrace();
+		}
 		numProdotti--;
 		stampaSituazione("Consumato " + idProdotto);
+
+		mag_vuoto = true; //magazzino vuoto
+		notify(); //sveglio il Produttore
+		
 		return idProdotto;
 	}
-	
+
 	private synchronized void stampaSituazione(String	msg) {
 		System.out.println(msg +"\n" + numProdotti + " prodotti in magazzino");
 	}
